0cca1cfa602a867589a9e7cb25c055df27836046
Chympe-explora <senlysuchiang87@gmail.com>
2026-06-28 14:34:48 +0530

    Add mobile menu toggle functionality

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector(".menu-btn");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("active");
+});
+Add script.js
